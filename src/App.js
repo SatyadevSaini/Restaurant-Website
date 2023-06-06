b609aa7c20041659edf98e45a45748c03657215b
Satyadev Saini <105744852+SatyadevSaini@users.noreply.github.com>
2023-06-06 12:31:41 +0530

    Cart Added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import Header from "./Components/Layout/Header";
 import MealsSummary from "./Components/Meals/MealsSummary";
 import AvailableMeals from "./Components/Meals/AvailableMeals";
+import Cart from "./Components/Cart/Cart";
 
 const App = () => {
  
   return (
      <Fragment>
+     <Cart/> 
         <Header/>
         <main>
           <MealsSummary/>
